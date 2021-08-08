1b101dc4947667080e91c965e620ea80e2050ac0
CyTon Code <cytoncode@gmail.com>
2021-08-08 14:02:11 +0000

    Update mount_this_linux.sh

--- mount_this_linux.sh
+++ mount_this_linux.sh
@@ -3,27 +3,31 @@ my_shell=bash
 
 
 # PATH:
-# my / partition:
+
+# my / Partition:
 my_dir=/mnt
 # my home work directory:
-home_dir=$my_dir/home/$user
+user_dir=$my_dir/home/$user
 # my backup directory with 16 GB:
-backup_dir=$home_dir/backup
+backup_dir=$user_dir/backup
+
+# MAKE DIRECTORY AND MOUNTING POINT:
 
 # if boot and home is not have then:
 mkdir $my_dir/{boot,home}
-
+# this Partition run first for booting:
 mount /dev/sda2 $my_dir/boot/
-
-swapon /dev/sda3 
-
+# this Partition - RAM but on hard-disk:
+swapon /dev/sda3 # SWAP
+# this Partition - home directory:
 mount /dev/sda5 $my_dir/home/
-
-
+# if backup is not have then:
 mkdir $backup_dir
+# I like to use this Partition to store important copies:
 mount /dev/sda6 $backup_dir/
-
+# RUN BASH ON YOUR HARD-DISK:
 arch-chroot $my_dir/
 
 # do backup bash history comands and others:
-cat $home_dir/.$my_shell'_history' >> backup_dir/.$my_shell'_history'
+# cat $user_dir/.$my_shell'_history' >> backup_dir/.$my_shell'_history'
+bash run_backup.sh
