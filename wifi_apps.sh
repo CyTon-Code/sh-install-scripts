b7785931df68b4f3f2e6f9d34b490be5d88b6e9d
CyTon Code <cytoncode@gmail.com>
2021-08-08 15:02:59 +0000

    Update and rename get_wifi_on_hard.sh to wifi_apps.sh

---
+++ wifi_apps.sh
@@ -0,0 +1,15 @@
+# PATH:
+
+# my / Partition:
+my_dir=/mnt
+
+
+# INSTALL/UPDATE WiFi apps on your linux-PK:
+
+# check this deamons:
+systemctl check systemd-networkd systemd-resolved iwd
+# install iwd servise if not found - inactive:
+pacstrap $my_dir/ iwd # and  systemd-networkd systemd-resolved
+# and run servise:
+systemctl start iwd # or enable - for start in botting
+
