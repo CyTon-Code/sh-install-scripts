31e1ea55a441b87d0aeed60f2e6ad4269da085ad
CyTon Code <cytoncode@gmail.com>
2021-08-08 14:06:20 +0000

    Update run_backup.sh

--- run_backup.sh
+++ run_backup.sh
@@ -1,2 +1,18 @@
-cat $user_dir/.$my_shell'_history' >> backup_dir/.$my_shell'_history'
+user=gnu
+my_shell=bash
+
+
+# PATH:
+
+# my / Partition:
+my_dir=/mnt
+# my home work directory:
+user_dir=$my_dir/home/$user
+# my backup directory with 16 GB:
+backup_dir=$user_dir/backup
+
+
+# BACKUP:
+
+cat $user_dir/.$my_shell'_history' >> $backup_dir/.$my_shell'_history'
 
