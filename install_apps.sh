b4c24fb8388e62f1dc0dcb7b30ef07bb81f7ab98
CyTon Code <cytoncode@gmail.com>
2021-08-08 12:58:12 +0000

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-bash get_wifi.sh
-pacman -Syy man less sed sudo cat
-bash get_git.sh
-bash get_micro.sh
+pacman -Syy man less cat sed sudo # base-devel cd ls echo  - base apps
+
+bash get_wifi.sh # get wifi conect for work in internet
+bash get_git.sh # get git for work with repo
+bash get_nano.sh #  or  get_micro.sh - get my like editor
 
 pacman -S wget w3m fff
 
