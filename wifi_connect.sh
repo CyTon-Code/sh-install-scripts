fdca2447d6c9d5979781c2dbbca8aed255e75023
CyTon Code <cytoncode@gmail.com>
2021-08-08 15:06:01 +0000

    Update wifi_connect.sh

--- wifi_connect.sh
+++ wifi_connect.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/etc bash
 
+
+# VALUES:
+
 your_WiFi="TP-LINK_2B62"
 wlan=wlan0
 phy=phy0
 
-# CONNECT on Life-Linux:
+
+# CONNECT:
 
 # What I do?:
 iwctl wsc $wlan start-pin
@@ -13,17 +17,7 @@ iwctl station $wlan scan
 # connect with your wifi-routers:
 iwctl station $wlan connect $your_WiFi
 
-# test wifi connect:
+
+# TEST YOUR WIFI CONNECT:
 ping -c 2 google.com
-
-
-
-# INSTALL/UPDATE WiFi apps on your linux-PK:
-
-# check this deamons:
-systemctl check systemd-networkd systemd-resolved iwd
-# install iwd servise if not found - inactive:
-pacstrap /mnt iwd # and  systemd-networkd systemd-resolved
-# and run servise:
-systemctl start iwd # or enable - for start in botting
 
