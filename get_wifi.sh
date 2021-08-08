988a02633ed822121ceb7cde87b30ccd0e9aaf83
CyTon Code <cytoncode@gmail.com>
2021-08-08 13:23:34 +0000

    Update get_wifi.sh

--- get_wifi.sh
+++ get_wifi.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/etc bash
 
 # INSTALL/UPDATE WiFi apps:
-pacman -Syy iwd
+
+# check this deamons:
+systemctl check systemd-networkd systemd-resolved iwd
+
+# install iwd servise if not found - inactive:
+castrap /mnt iwd # and  systemd-networkd systemd-resolved
+
 systemctl start iwd # or enable - for start in botting
 
 # CONNECT:
@@ -11,8 +17,8 @@ iwctl wsc wlan0 start-pin
 # scan anything wifi for connect:
 iwctl station wlan0 scan
 # connect with your wifi-routers:
-iwctl station wlan0 connect TP-LINK_2B62
+iwctl station wlan0 connect Name_Your_WiFi
 
 # test wifi connect:
-ping -C 4 google.com
+ping -c 4 google.com
 
