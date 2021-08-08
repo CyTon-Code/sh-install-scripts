bb4f0f6c3234dc550980fd82f8251ecfa990a38e
CyTon Code <cytoncode@gmail.com>
2021-08-08 11:56:52 +0000

    Update get_wifi.h

--- get_wifi.h
+++ get_wifi.h
@@ -1,6 +1,11 @@
 #!/usr/bin/etc bash
+# What I do?:
 iwctl wsc wlan0 start-pin
+# scan anything wifi for connect:
 iwctl station wlan0 scan
+# connect with your wifi-routers:
 iwctl station wlan0 connect TP-LINK_2B62
 
-ping 8.8.8.8
+# test wifi connect:
+ping -C 4 8.8.8.8
+
