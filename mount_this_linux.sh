45cf957453c2598d5e7cab4c2b1689139970f4dc
CyTon Code <cytoncode@gmail.com>
2021-08-08 11:49:55 +0000

    Update mount_this_linux.sh

--- mount_this_linux.sh
+++ mount_this_linux.sh
@@ -1,12 +1,29 @@
-mkdir /mnt/{boot,home}
-mount /dev/sda2 /mnt/boot
+user=gnu
+my_shell=bash
 
-swapon /dev/sda3
 
-mount /dev/sda5 /mnt/home
+# PATH:
+# my / partition:
+my_dir=/mnt
+# my home work directory:
+home_dir=$my_dir/home/$user
+# my backup directory with 16 GB:
+backup_dir=$home_dir/backup
 
-mkdir /mnt/gnu/Backup
-mount /dev/sda6 /mnt/home/gnu/Backup
+# if boot and home is not have then:
+mkdir $my_dir/{boot,home}
 
-arch-chroot /mnt
+mount /dev/sda2 $my_dir/boot/
 
+swapon /dev/sda3 
+
+mount /dev/sda5 $my_dir/home/
+
+
+mkdir $backup_dir
+mount /dev/sda6 $backup_dir/
+
+arch-chroot $my_dir/
+
+# do backup bash history comands and others:
+cat $home_dir/.$my_shell'_history' > backup_dir/.$my_shell'_history'
